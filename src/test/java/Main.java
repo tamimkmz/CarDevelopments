b7cc636d522fa05dc375855d9238da7f72c2c7cc
tamimkmz <tamim.kmz@gmail.com>
2023-11-04 15:23:23 -0700

    basic sturcture of the audi is ready

---
+++ src/test/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        carFeatures audi = new carFeatures();
+        audi.Color = "black";
+        audi.Hp = "5000";
+        audi.Doors = "2";
+        audi.Model = "A90";
+    }
+}
---
+++ src/test/java/carFeatures.java
@@ -0,0 +1,9 @@
+public class carFeatures {
+    String Doors;
+    String Color;
+    String Hp;
+    String Model;
+
+}
+
+
